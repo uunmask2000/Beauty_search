9d0a803cdb388f529beaf25dc4043bca5e4c7381
DESKTOP-K5CB2H4\git <uunmask2000@gmail.com>
2019-06-25 14:45:19 +0800

    add

--- python/ck101/rub_ck101.py
+++
@@ -1,8 +0,0 @@
-import os
-url = 'https://ck101.com/forum.php?mod=forumdisplay&fid=3581&page={}'
-for row in range(1, 50):
-    url_ = url.format(row)
-    cmd = 'start C:\\Users\\uunma\\AppData\\Local\\Programs\\Python\\Python37-32\\python.exe ' + \
-        "C:\PJ\卡提若\ck101.py " + "" + str(row) + "" 
-    # print(cmd)
-    os.system(cmd)
